4e5244b604e29cbf524ae7c91f88705826c245e9
Tair Kaldybayev <taircaldy.yt@gmail.com>
2025-10-06 22:04:04 +0200

    small changes in Hero and Contact sections: Main Text and terminal image change and deletion

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
   return (
     <section className='c-space my-20' id="contact">
         <div className='relative min-h-screen flex items-center justify-center flex-col'>
-            <img src="/assets/terminal.png" alt="terminal background" className='absolute inset-0 min-h-screen' />
+            {/* <img src="/assets/terminal.png" alt="terminal background" className='absolute inset-0 min-h-screen' /> */}
 
             <div className='contact-container'>
                 <h3 className='head-text mt-10'>Let's Talk</h3>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
                 <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>
                     Hi, I'm Tair <span className='waving-hand'>👋</span>
                 </p>
-                <p className='hero_tag text-gray_gradient'> Building IT Products & Solutions</p>
+                <p className='hero_tag text-gray_gradient'> Software Engineer & Founder </p>
             </div>
 
             <div className='w-full h-full absolute inset-0'>
